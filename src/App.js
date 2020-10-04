4d85ebae3be02749c3f0b352a5f42ce3b85361c9
Alison Clayton <53766022+alisonclayton@users.noreply.github.com>
2020-10-04 19:43:24 -0300

    up ./src content 001

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// import Landing from './components/Landing'
+// import Header from './components/Header'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Landing /> */}
+      {/* <Header /> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
